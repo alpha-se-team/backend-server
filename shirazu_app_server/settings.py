529f166650c69f4f9ef8ef5d9e9e8a1faedc0716
alifarazz <alifarazdaghi@gmail.com>
2019-06-12 21:37:03 +0430

    Enable HTTP method override

--- shirazu_app_server/settings.py
+++ shirazu_app_server/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middlewares.MethodOverrideMiddleware',
 ]
 
 ROOT_URLCONF = 'shirazu_app_server.urls'
